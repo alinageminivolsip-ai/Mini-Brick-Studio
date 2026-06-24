a781f8f0c1793848ba14f18ba1b919f80219a1a3
alinageminivolsip-ai <alinageminivolsip@gmail.com>
2026-06-24 10:25:41 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME = 'minibrick-3d-v1';
-const FILES = ['./','./index.html','./manifest.json','./assets/logo.png','./assets/icons/icon-192.png','./assets/icons/icon-512.png'];
-self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES))); self.skipWaiting(); });
-self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
-self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
