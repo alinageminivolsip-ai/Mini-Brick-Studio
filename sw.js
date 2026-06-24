322dbd5393ef881c217c6698ded57ad601c1dfe5
alinageminivolsip-ai <alinageminivolsip@gmail.com>
2026-06-24 11:42:15 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'minibrick-layered-png-v1';
+const CACHE_NAME = 'minibrick-layered-png-v2';
 const ASSETS = ['./','./index.html','./manifest.json','./logo.png','./icon-192.png','./icon-512.png','./assets/body/skin-mask.png','./assets/body/torso-mask.png','./assets/body/legs-mask.png','./assets/body/base-shadow.png','./assets/faces/A1.png','./assets/hair/1.png'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))).then(()=>self.clients.claim())));
